f3b81787822a91fcd17b83bc7059db2aaeb94a43
ThalissonS <thalissonpro1@gmail.com>
2025-08-09 23:03:33 -0300

    Adiciona script para criação de tabelas no deploy

---
+++ create_tables.py
@@ -0,0 +1,7 @@
+# create_tables.py
+from app import app, db
+
+with app.app_context():
+    print("Criando todas as tabelas do banco de dados...")
+    db.create_all()
+    print("Tabelas criadas com sucesso!")
